d044af5bb22219573d5e6d9ac3faaf0a38442fdd
rndigkwk <49711707+rndigkwk@users.noreply.github.com>
2026-08-10 11:03:57 +0900

    [프로그래머스] L0 - 문자열의 앞의 n글자 (#116)

---
+++ programmers/L0_기초입문/기초트레이닝/116_문자열의_앞의_n글자.py
@@ -0,0 +1,10 @@
+# 문자열의 앞의 n글자
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181907
+# 알고리즘: 문자열
+# 작성자: 학생
+# 작성일: 2026. 08. 10. 11:03:55
+
+def solution(my_string, n):
+    answer = my_string[:n]
+    return answer
