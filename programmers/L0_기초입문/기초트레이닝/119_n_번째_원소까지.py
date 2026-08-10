a96249e0b9314cfd52a50538876de9370e6b250a
rndigkwk <49711707+rndigkwk@users.noreply.github.com>
2026-08-10 10:20:33 +0900

    [프로그래머스] L0 - n 번째 원소까지 (#119)

---
+++ programmers/L0_기초입문/기초트레이닝/119_n_번째_원소까지.py
@@ -0,0 +1,10 @@
+# n 번째 원소까지
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181889
+# 알고리즘: 리스트(배열)
+# 작성자: 학생
+# 작성일: 2026. 08. 10. 10:20:31
+
+def solution(num_list, n):
+    answer = num_list[:n]
+    return answer
