669b0e3c2284eba4451cfb05e27c106c8e4768fc
rndigkwk <49711707+rndigkwk@users.noreply.github.com>
2026-08-10 10:33:22 +0900

    [프로그래머스] L0 - 문자열 돌리기 (#124)

---
+++ programmers/L0_기초입문/기초트레이닝/124_문자열_돌리기.py
@@ -0,0 +1,10 @@
+# 문자열 돌리기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181945
+# 알고리즘: 출력
+# 작성자: 학생
+# 작성일: 2026. 08. 10. 10:33:20
+
+str = input()
+for i in range(len(str)):
+    print(f'{str[i]}')
