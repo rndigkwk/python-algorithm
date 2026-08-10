30e6845b3144df8d1161165c8f8f23a58f264f09
rndigkwk <49711707+rndigkwk@users.noreply.github.com>
2026-08-10 10:37:50 +0900

    [프로그래머스] L0 - 문자열 붙여서 출력하기 (#105)

---
+++ programmers/L0_기초입문/기초트레이닝/105_문자열_붙여서_출력하기.py
@@ -0,0 +1,9 @@
+# 문자열 붙여서 출력하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181946
+# 알고리즘: 출력
+# 작성자: 학생
+# 작성일: 2026. 08. 10. 10:37:48
+
+str1, str2 = input().strip().split(' ')
+print(str1+str2)
